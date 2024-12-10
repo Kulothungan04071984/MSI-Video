be651cf240347b2ff1224ce2a017ee6ee4c42abe
Ajith-syrma <ajith.m@syrmasgs.com>
2024-12-10 16:59:09 +0530

    Login Page

---
+++ MSI/Models/UserLogin.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MSI.Models;
+public partial class UserLogin
+{
+    public int Userloginid { get; set; }
+
+    public string? Userid { get; set; }
+
+    public string? Userpassword { get; set; }
+
+    public string? Usertype { get; set; }
+
+    public int? Createid { get; set; }
+
+    public DateTime? Createddate { get; set; }
+
+    public int? Updateid { get; set; }
+
+    public DateTime? Updatedate { get; set; }
+
+    public bool? Isactive { get; set; }
+}
