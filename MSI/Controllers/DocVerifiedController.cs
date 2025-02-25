4093da08a378a80728b32212e373e95a7e38a970
kulothungan04071984 <kulothungan.k@syrmasgs.com>
2025-02-25 12:39:13 +0530

    Create Doc Verified Page

---
+++ MSI/Controllers/DocVerifiedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSI.Controllers
+{
+    public class DocVerifiedController : Controller
+    {
+        public IActionResult ShowDocDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ MSI/Models/DocVerified.cs
@@ -0,0 +1,15 @@
+﻿namespace MSI.Models
+{
+    public class DocVerified
+    {
+        public int docId {  get; set; }
+        public string empId { get; set; }
+        public string empName { get; set; }
+        public string docName { get; set; }
+        public string docDateTime { get; set; }
+        public string docType { get; set; }
+        public string docStatus { get; set; }
+
+        public bool isActive { get; set; }
+    }
+}
